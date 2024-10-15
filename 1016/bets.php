eeac96573c852c932f8e7bad38ff8260cbb299db
artistic5 <artisticsoftware5@gmail.com>
2024-10-15 04:26:08 +0000

    ...

--- 1016/bets.php
+++ 1016/bets.php
@@ -5,40 +5,40 @@ return [
 		/**
 		Race 1
 		*/
-		'favorites' => '8',
+		'favorites' => '5, 8',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'suggestions' => [
-			'win' => '',
-			'qin' => '',
-			'trio' => '',//count trio: 0
-			'inter' => '',
+			'win' => '5, 8',
+			'qin' => '2, 5, 8, 9',
+			'trio' => '2, 3, 5, 7, 8, 9, 12',//count trio: 7
+			'inter' => '5, 8',
 		],
-		'win inter' => '1, 3, 4, 5, 8',
+		'win inter' => '1, 4, 5, 8',
+		'win inter 2' => '5, 8',
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
 		'bets' => [
 		],
 		'total bets' => 0,
-		'wp' => '8',//count wp: 1
+		'wp' => '5, 8',//count wp: 2
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'favorites' => '1, 5',
+		'favorites' => '1, 5, 7',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'suggestions' => [
-			'win' => '5, 6',
-			'qin' => '2, 5, 6, 11',
-			'trio' => '2, 3, 5, 6, 11',//count trio: 5
-			'inter' => '5',
+			'win' => '2, 4, 5, 6, 7, 8, 9',
+			'qin' => '2, 4, 5, 6, 7, 8, 9, 11, 12',
+			'trio' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',//count trio: 12
+			'inter' => '5, 7',
 		],
-		'win inter' => '4, 5, 6, 9, 10',
-		'win inter 2' => '5, 6',
+		'win inter' => '4, 6, 9',
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
 		'bets' => [
 		],
 		'total bets' => 0,
-		'wp' => '1, 5',//count wp: 2
+		'wp' => '1, 5, 7',//count wp: 3
 	],
 	'3' => [
 		/**
@@ -121,20 +121,21 @@ return [
 		/**
 		Race 7
 		*/
-		'favorites' => '2, 4',
+		'favorites' => '2, 4, 12',
 		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12',
 		'suggestions' => [
 			'win' => '2, 4',
-			'qin' => '1, 2, 4',
-			'trio' => '1, 2, 4, 5, 8',//count trio: 5
+			'qin' => '1, 2, 4, 7',
+			'trio' => '1, 2, 4, 5, 7, 8',//count trio: 6
 			'inter' => '2, 4',
 		],
-		'win inter' => '1, 2, 4, 7, 10',
-		'win inter 2' => '2, 4',
+		'win inter' => '2',
+		'win inter 2' => '2',
 		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
 		'bets' => [
+			'place(end-favorites)' => '12',
 		],
-		'total bets' => 0,
+		'total bets' => 100,
 		'wp' => '2, 4',//count wp: 2
 	],
 	'8' => [
@@ -158,7 +159,7 @@ return [
 		'wp' => '5, 8',//count wp: 2
 	],
 ];
-//total place end favorites: 0
+//total place end favorites: -100
 //total place end wp: 0
 //total place wp: 0
 //total sure place: 0
