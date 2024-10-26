931c3451e59d765ad39fa51ef5f642255b0b7ec6
artistic6 <artisticsoftware6@gmail.com>
2024-10-26 05:11:37 +0000

    ...

---
+++ 1027S1/bets17.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '6, 12',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+		],
+		'win inter' => '1, 7',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+			'win(union 17)' => '1, 6, 7, 12',
+			'win(experimental 17)' => '1, 6, 7, 12',
+		],
+		'total bets' => 400,
+		'wp' => '6',//count wp: 1
+	],
+];
+//total place end favorites: 0
+//total place end wp: 0
+//total place wp: 0
+//total sure place: 0
+//total win: -400
+//total: 0
---
+++ 1027S1/bets20.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '6, 12',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+		],
+		'win inter' => '1, 7, 12',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '6',//count wp: 1
+	],
+];
+//total place end favorites: 0
+//total place end wp: 0
+//total place wp: 0
+//total sure place: 0
+//total win: 0
+//total: 0
